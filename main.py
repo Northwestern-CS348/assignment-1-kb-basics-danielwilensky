6f84fc916ceb927b78480babd4dbe134f3994d1f
danielwilensky <danielwilensky2019@u.northwestern.edu>
2019-01-24 19:59:35 -0600

    Finished HW1

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ class KBTest(unittest.TestCase):
         ask1 = read.parse_input("fact: (color bigbox red)")
         print(' Asking if', ask1)
         answer = self.KB.kb_ask(ask1)
+        print("This is: ")
+        print(answer)
         self.assertEqual(answer[0].bindings, [])
         #self.assertEqual(answer.list_of_bindings[0][1][0], ask1)
 
@@ -26,12 +28,16 @@ class KBTest(unittest.TestCase):
         ask1 = read.parse_input("fact: (color littlebox red)")
         print(' Asking if', ask1)
         answer = self.KB.kb_ask(ask1)
+        print("This is: ")
+        print(answer)
         self.assertFalse(answer)
 
     def test3(self):
         ask1 = read.parse_input("fact: (color ?X red)")
         print(' Asking if', ask1)
         answer = self.KB.kb_ask(ask1)
+        print("This is: ")
+        print(answer)
         self.assertEqual(str(answer[0]), "?X : bigbox")
         self.assertEqual(str(answer[1]), "?X : pyramid3")
         self.assertEqual(str(answer[2]), "?X : pyramid4")
@@ -53,7 +59,44 @@ class KBTest(unittest.TestCase):
         self.assertEqual(str(answer[3]), "?X : pyramid2, ?Y : green")
         self.assertEqual(str(answer[4]), "?X : pyramid3, ?Y : red")
         self.assertEqual(str(answer[5]), "?X : pyramid4, ?Y : red")
-        
+
+    def test6(self):
+        state = Statement(['color','door','red'])
+        item = Fact(state)
+        self.KB.kb_assert(item)
+        ask1 = read.parse_input("fact: (color door red)")
+        print(' Asking if', ask1)
+        answer = self.KB.kb_ask(ask1)
+        self.assertTrue(answer)
+
+    def test7(self):
+        ask1 = read.parse_input("fact: (inst ?X pyramid)")
+        print(' Asking if', ask1)
+        answer = self.KB.kb_ask(ask1)
+        self.assertEqual(str(answer[0]), "?X : pyramid1")
+        self.assertEqual(str(answer[1]), "?X : pyramid2")
+        self.assertEqual(str(answer[2]), "?X : pyramid3")
+        self.assertEqual(str(answer[3]), "?X : pyramid4")
+
+    def test8(self):
+        state = Statement(['inst','pyramid1','pyramid'])
+        item = Fact(state)
+        self.KB.kb_assert(item)
+        ask1 = read.parse_input("fact: (inst ?X pyramid)")
+        print(' Asking if', ask1)
+        answer = self.KB.kb_ask(ask1)
+        self.assertEqual(len(answer), 4)
+
+    def test9(self):
+        self.KB.kb_assert('coolio')
+        ask1 = read.parse_input("fact: (inst ?X pyramid)")
+        print(' Asking if', ask1)
+        answer = self.KB.kb_ask(ask1)
+        self.assertEqual(len(answer), 4)
+
+    def test10(self):
+        answer = self.KB.kb_ask('h')
+        self.assertFalse(answer)
 
 if __name__ == '__main__':
     unittest.main()
--- student_code.py
+++ student_code.py
@@ -7,15 +7,18 @@ class KnowledgeBase(object):
     def __init__(self, facts=[], rules=[]):
         self.facts = facts
         self.rules = rules
+        #to create new KB: kb1 = KnowledgeBase(facts=[],rules=[])
 
     def __repr__(self):
         return 'KnowledgeBase({!r}, {!r})'.format(self.facts, self.rules)
+        #can call just with repr(kb1)
 
     def __str__(self):
         string = "Knowledge Base: \n"
         string += "\n".join((str(fact) for fact in self.facts)) + "\n"
         string += "\n".join((str(rule) for rule in self.rules))
         return string
+        #can call just with str(kb1)
 
     def kb_assert(self, fact):
         """Assert a fact or rule into the KB
@@ -24,6 +27,14 @@ class KnowledgeBase(object):
             fact (Fact or Rule): Fact or Rule we're asserting in the format produced by read.py
         """
         print("Asserting {!r}".format(fact))
+        alreadyin = False
+        for i in range(len(self.facts)):
+            if self.facts[i] == fact:
+                alreadyin = True
+                break
+        if isinstance(fact, Fact): #better than fact.name == 'fact'
+            if alreadyin == False:
+                self.facts.append(fact)
         
     def kb_ask(self, fact):
         """Ask if a fact is in the KB
@@ -35,3 +46,13 @@ class KnowledgeBase(object):
             ListOfBindings|False - ListOfBindings if result found, False otherwise
         """
         print("Asking {!r}".format(fact))
+        if isinstance(fact, Fact) == False:
+            return False
+        bindings = ListOfBindings()
+        for i in range(len(self.facts)):
+            binding = match(self.facts[i].statement,fact.statement)
+            if binding != False:
+                bindings.add_bindings(binding)
+        if len(bindings) == 0:
+            return False
+        return bindings
